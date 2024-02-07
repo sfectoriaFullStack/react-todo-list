e79681dc2fce33ec78a625bfd42272f86717b643
khalil kraiem <khalilmedkraiem@gmail>
2024-02-07 17:50:21 +0100

    class component

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,14 +1,42 @@
 import React from "react";
 
-function AddItem({ value }) {
+function AddItem({ elem,i,state,setState}) {
   return (
     <div className="d-flex justify-content-between p-2">
-      <div className="d-flex align-items-center">
-        <input type="checkbox" />
-        <p className="m-0">{value}</p>
-      </div>
-      <button className="btn btn-danger">Delete</button>
+    <div className="d-flex align-items-center">
+      <input
+        checked={elem.checked}
+        type="checkbox"
+        onChange={(e) => {
+          let array = [...state.tasks];
+          array[i].checked = e.target.checked;
+          setState({
+            tasks: array,
+          });
+        }}
+      />
+      <p
+        className="m-0"
+        style={{
+          textDecoration: elem.checked ? "line-through" : "none",
+        }}
+      >
+        {elem.value}
+      </p>
     </div>
+    <button
+      className="btn btn-danger"
+      onClick={() => {
+        setState({
+          tasks: state.tasks.filter(
+            (el, j) => elem.id !== el.id
+          ),
+        });
+      }}
+    >
+      Delete
+    </button>
+  </div>
   );
 }
 
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -5,38 +5,53 @@ export default class Test extends Component {
   constructor() {
     super();
     this.state = {
-      tasks: [
-        { id: 1, value: "seif", checked: false },
-        { id: 2, value: "rawan ma tetghachech", checked: false },
-        { id: 3, value: "malek dima nice", checked: false },
-      ],
+      value: "",
+      tasks: [],
     };
+    this.setState=this.setState.bind(this)
+  }
+  componentDidMount(){
+   let list =localStorage.getItem('tasks')
+   if(list){
+    this.setState({tasks:JSON.parse(list)})
+   }
   }
   render() {
     return (
       <div className="d-flex justify-content-center">
         <div>
           <div className="d-flex">
-            <input className="form-control" />
+            <input
+              className="form-control"
+              onChange={(e) => this.setState({ value: e.target.value })}
+            />
             <button
               className="btn btn-primary"
               onClick={() => {
                 console.log("hello");
+                let lastId=localStorage.getItem('lastId')
                 this.setState({
                   tasks: [
                     ...this.state.tasks,
-                    { id: 4, value: "malek dima nice", checked: false },
+                    { id: +lastId+1, value: this.state.value, checked: false },
                   ],
                 });
+                localStorage.setItem(
+                  "tasks",
+                  JSON.stringify([
+                    ...this.state.tasks,
+                    { id: +lastId+1, value: this.state.value, checked: false },
+                  ])
+                );
+                localStorage.setItem('lastId',+lastId+1)
               }}
             >
               Add task
             </button>
-           
           </div>
           {this.state.tasks.map((elem, i) => (
             <div key={i}>
-              <AddItem value={elem.value} />
+             <AddItem elem={elem} i={i} state={this.state} setState={this.setState}/> 
             </div>
           ))}
         </div>
